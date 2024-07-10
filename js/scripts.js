95084f86d59ba20764109c5b1769a2f1dbda912d
Terry Arthur Brooks, Jr <Terry.Arthur@BrooksJr.com>
2024-07-09 21:41:41 -0400

    Update date

--- js/scripts.js
+++ js/scripts.js
@@ -2262,7 +2262,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	
 	// Set up FlipDown
-	var flipdown = new FlipDown(1721048400)
+	var flipdown = new FlipDown(1722502800)
   
 	  // Start the countdown
 	  .start()
